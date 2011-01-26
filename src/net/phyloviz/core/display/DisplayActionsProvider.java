d42b2cee9c94db8f5a61b0ee38c5f5f200ab44e8
Alexandre P. Francisco <aplf@ist.utl.pt>
2011-01-26 21:24:38 +0000

    Add display options tracking.

---
+++ src/net/phyloviz/core/display/DisplayActionsProvider.java
@@ -0,0 +1,9 @@
+package net.phyloviz.core.display;
+
+import java.util.Collection;
+import javax.swing.Action;
+
+public interface DisplayActionsProvider {
+
+	public Collection<Action> getDisplayActions();
+}
--- src/net/phyloviz/core/display/DisplayOptionsAction.java
+++ src/net/phyloviz/core/display/DisplayOptionsAction.java
@@ -1,28 +1,48 @@
 package net.phyloviz.core.display;
 
 import java.awt.event.ActionEvent;
+import java.awt.event.KeyEvent;
+import java.util.Iterator;
 import javax.swing.AbstractAction;
+import javax.swing.Action;
 import javax.swing.JMenu;
 import javax.swing.JMenuItem;
 import org.openide.util.actions.Presenter;
+import org.openide.windows.TopComponent;
 
-public final class DisplayOptionsAction extends AbstractAction implements Presenter.Menu {
-
-	@Override
-	public void actionPerformed(ActionEvent e) {
-		// TODO implement action body
-	}
+public final class DisplayOptionsAction extends AbstractAction implements Presenter.Popup, Presenter.Menu {
 
 	@Override
-	public JMenuItem getMenuPresenter() {
-		JMenu result = new JMenu();
+	public JMenuItem getPopupPresenter() {
 
-		result.add(new JMenuItem("Ok1"));
-		result.add(new JMenuItem("Ok2"));
+		JMenu result = new JMenu("Display Options");
+		result.setMnemonic(KeyEvent.VK_D);
+		TopComponent tc = TopComponent.getRegistry().getActivated();
+
+		if (tc instanceof DisplayActionsProvider) {
+			Iterator<Action> ia = ((DisplayActionsProvider) tc).getDisplayActions().iterator();
+			while (ia.hasNext()) {
+				JMenuItem mi = new JMenuItem();
+				Action a = ia.next();
+				mi.setText(a.toString());
+				mi.addActionListener(a);
+				result.add(mi);
+			}
+		}
+
+		if (result.getItemCount() == 0)
+			result.setEnabled(false);
 
 		return result;
 	}
 
+	@Override
+	public JMenuItem getMenuPresenter() {
+		return getPopupPresenter();
+	}
 
-
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// No action.
+	}
 }
