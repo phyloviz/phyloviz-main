5ad15e066ddad28056e380a764a791bd56bcbafe
Alexandre P. Francisco <aplf@ist.utl.pt>
2011-02-03 14:01:39 +0000

    Increment implementation version.

--- src/net/phyloviz/category/CategoryProvider.java
+++ src/net/phyloviz/category/CategoryProvider.java
@@ -7,7 +7,6 @@ import java.util.Comparator;
 import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
-import java.util.Map;
 import java.util.Map.Entry;
 import java.util.TreeMap;
 import java.util.TreeSet;
