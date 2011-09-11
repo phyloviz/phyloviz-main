95f94e2e0ee4182d98665cb15c5ab5a20fb13670
Pedro Tiago Monteiro <pedro@pedromonteiro.org>
2011-09-11 13:12:21 +0100

    NEW: initial commit of pubmlst module for PHYLOViZ to load pubmlst.org data through the SOAP API.

---
+++ src/net/phyloviz/pubmlst/wizard/PubMLSTWizardAction.java
@@ -0,0 +1,90 @@
+package net.phyloviz.pubmlst.wizard;
+
+import java.awt.Color;
+import java.awt.Component;
+import java.awt.Dialog;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.text.MessageFormat;
+import javax.swing.JComponent;
+import org.openide.DialogDisplayer;
+import org.openide.WizardDescriptor;
+import org.openide.WizardDescriptor.Panel;
+import org.openide.util.HelpCtx;
+
+public final class PubMLSTWizardAction implements ActionListener {
+
+	private WizardDescriptor.Panel[] panels;
+
+	@SuppressWarnings("unchecked")
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		WizardDescriptor wizardDescriptor = new WizardDescriptor((Panel<WizardDescriptor>[]) getPanels());
+		// {0} will be replaced by WizardDesriptor.Panel.getComponent().getName()
+		wizardDescriptor.setTitleFormat(new MessageFormat("{0}"));
+		wizardDescriptor.setTitle("Load PubMLST Database");
+		wizardDescriptor.putProperty("WizardPanel_contentBackgroundColor", Color.WHITE);
+		//wizardDescriptor.putProperty("WizardPanel_image", ImageUtilities.loadImage("net/phyloviz/core/wizard.gif", true));
+		wizardDescriptor.putProperty("WizardPanel_imageAligment", "center");
+		Dialog dialog = DialogDisplayer.getDefault().createDialog(wizardDescriptor);
+		dialog.setVisible(true);
+		dialog.toFront();
+		boolean cancelled = wizardDescriptor.getValue() != WizardDescriptor.FINISH_OPTION;
+		if (!cancelled) {
+			// do something
+		}
+	}
+
+	/**
+	 * Initialize panels representing individual wizard's steps and sets
+	 * various properties for them influencing wizard appearance.
+	 */
+	private WizardDescriptor.Panel[] getPanels() {
+		if (panels == null) {
+			panels = new WizardDescriptor.Panel[]{
+				new PubMLSTWizardPanel1(),
+				new PubMLSTWizardPanel2(),
+				new PubMLSTWizardPanel3()
+				};
+			String[] steps = new String[panels.length];
+			for (int i = 0; i < panels.length; i++) {
+				Component c = panels[i].getComponent();
+				// Default step name to component name of panel. Mainly useful
+				// for getting the name of the target chooser to appear in the
+				// list of steps.
+				steps[i] = c.getName();
+				if (c instanceof JComponent) { // assume Swing components
+					JComponent jc = (JComponent) c;
+					// Sets step number of a component
+					// TODO if using org.openide.dialogs >= 7.8, can use WizardDescriptor.PROP_*:
+					jc.putClientProperty("WizardPanel_contentSelectedIndex", new Integer(i));
+					// Sets steps names for a panel
+					jc.putClientProperty("WizardPanel_contentData", steps);
+					// Turn on subtitle creation on each step
+					jc.putClientProperty("WizardPanel_autoWizardStyle", Boolean.TRUE);
+					// Show steps on the left side with the image on the background
+					jc.putClientProperty("WizardPanel_contentDisplayed", Boolean.TRUE);
+					// Turn on numbering of all steps
+					jc.putClientProperty("WizardPanel_contentNumbered", Boolean.TRUE);
+				}
+			}
+		}
+		return panels;
+	}
+
+	public String getName() {
+		return "Start Sample Wizard";
+	}
+
+	public String iconResource() {
+		return null;
+	}
+
+	public HelpCtx getHelpCtx() {
+		return HelpCtx.DEFAULT_HELP;
+	}
+
+	protected boolean asynchronous() {
+		return false;
+	}
+}
---
+++ src/net/phyloviz/pubmlst/wizard/PubMLSTWizardPanel2.java
@@ -0,0 +1,93 @@
+package net.phyloviz.pubmlst.wizard;
+
+import java.awt.Component;
+import java.io.IOException;
+import javax.swing.event.ChangeListener;
+import net.phyloviz.core.data.AbstractProfile;
+import net.phyloviz.core.data.TypingData;
+import net.phyloviz.mlst.MLSTypingFactory;
+import org.openide.WizardDescriptor;
+import org.openide.WizardValidationException;
+import org.openide.util.HelpCtx;
+import org.openide.util.ImageUtilities;
+
+public class PubMLSTWizardPanel2 implements WizardDescriptor.ValidatingPanel {
+
+	/**
+	 * The visual component that displays this panel. If you need to access the
+	 * component from this class, just use getComponent().
+	 */
+	private String sDBShort;
+	private String sDBFull;
+	private Component component;
+	private MLSTypingFactory tf;
+	private TypingData<? extends AbstractProfile> td;
+
+	// Get the visual component for the panel. In this template, the component
+	// is kept separate. This can be more efficient: if the wizard is created
+	// but never displayed, or not all panels are displayed, it is better to
+	// create only those which really need to be visible.
+	@Override
+	public Component getComponent() {
+		if (component == null) {
+			component = new PubMLSTVisualPanel2();
+		}
+		return component;
+	}
+
+	@Override
+	public HelpCtx getHelp() {
+		// Show no Help button for this panel:
+		return HelpCtx.DEFAULT_HELP;
+	}
+
+	@Override
+	public boolean isValid() {
+		return true;
+	}
+
+	@Override
+	public final void addChangeListener(ChangeListener l) {
+	}
+
+	@Override
+	public final void removeChangeListener(ChangeListener l) {
+	}
+
+	// You can use a settings object to keep track of state. Normally the
+	// settings object will be the WizardDescriptor, so you can use
+	// WizardDescriptor.getProperty & putProperty to store information entered
+	// by the user.
+	@Override
+	public void readSettings(Object settings) {
+		((WizardDescriptor) settings).putProperty("WizardPanel_image", ImageUtilities.loadImage("net/phyloviz/core/TypingImage.png", true));
+		sDBShort = (String) ((WizardDescriptor) settings).getProperty("dbShort");
+		sDBFull = (String) ((WizardDescriptor) settings).getProperty("dbFull");
+		((PubMLSTVisualPanel2) getComponent()).setDatabase(sDBShort, sDBFull);
+		tf = new MLSTypingFactory();
+	}
+
+	@Override
+	public void storeSettings(Object settings) {
+		((WizardDescriptor) settings).putProperty("typing_factory", tf);
+		((WizardDescriptor) settings).putProperty("typing_data", td);
+	}
+
+	@Override
+	public void validate() throws WizardValidationException {
+		if (tf == null) {
+			throw new WizardValidationException(null, "Could not load MSLT plugin", null);
+		}
+
+		if (((PubMLSTVisualPanel2) getComponent()).isEmpty()) {
+			throw new WizardValidationException(null, "No typing data completely loaded yet!", null);
+		}
+
+		try {
+			td = tf.loadData(((PubMLSTVisualPanel2) getComponent()).getTypingData());
+			td.setDescription(tf.toString());
+		} catch (IOException ex) {
+			throw new WizardValidationException(null, "Fatal error: " + ex.getMessage(), null);
+		}
+	}
+}
