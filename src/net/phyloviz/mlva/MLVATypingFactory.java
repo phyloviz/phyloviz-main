9e142816adbf573ed475935b736bc28d999968fa
Alexandre P Francisco <aplf@ist.utl.pt>
2011-09-14 10:17:19 +0100

    Add sanity check while loading data.

--- src/net/phyloviz/mlva/MLVATypingFactory.java
+++ src/net/phyloviz/mlva/MLVATypingFactory.java
@@ -68,7 +68,11 @@ public class MLVATypingFactory implements TypingFactory {
 		TypingData<MLVAType> td = null;
 
 		while (in.ready()) {
-			String[] lineFields = in.readLine().split("[ ,\t]+", 0);
+			String s = in.readLine();
+			if (s == null)
+				break;
+
+			String[] lineFields = s.split("[ ,\t]+", 0);
 
 			// Get headers and initialize this instance.
 			if (td == null) {
