2392e47f190e44864b44f57a3e5539f8f4f4f1e3
Alexandre P Francisco <aplf@ist.utl.pt>
2011-12-14 23:38:37 +0000

    Add warning message if errors detected on profile loading.

--- src/net/phyloviz/mlst/MLSTypingFactory.java
+++ src/net/phyloviz/mlst/MLSTypingFactory.java
@@ -48,6 +48,8 @@ import net.phyloviz.core.data.Isolate;
 import net.phyloviz.core.data.Population;
 import net.phyloviz.core.data.TypingData;
 import net.phyloviz.core.util.TypingFactory;
+import org.openide.DialogDisplayer;
+import org.openide.NotifyDescriptor;
 import org.openide.util.lookup.ServiceProvider;
 
 @ServiceProvider(service = TypingFactory.class)
@@ -65,6 +67,7 @@ public class MLSTypingFactory implements TypingFactory {
 
 		BufferedReader in = new BufferedReader(r);
 		int uid = 0;
+		boolean error = false;
 
 		TypingData<SequenceType> td = null;
 
@@ -92,6 +95,7 @@ public class MLSTypingFactory implements TypingFactory {
 				if (!profile.getID().equals(oldProfile.getID())) {
 					Logger.getLogger(MLSTypingFactory.class.getName()).log(Level.WARNING,
 						"Duplicated profile: {0} aka {1} (frequency updated)", new Object[]{profile.getID(), oldProfile.getID()});
+					error = true;
 				}
 			} else {
 				try {
@@ -99,11 +103,18 @@ public class MLSTypingFactory implements TypingFactory {
 				} catch(Exception e) {
 					Logger.getLogger(MLSTypingFactory.class.getName()).log(Level.WARNING,
 						e.getLocalizedMessage());
+					error = true;
 				}
 			}
 		}
 		in.close();
 
+		if (error) {
+			String failMsg = "Some profiles may have been discarded! Check the log (View->Log).";
+			DialogDisplayer.getDefault().notify(new NotifyDescriptor.Message(failMsg));
+		}
+
+		
 		return td;
 	}
 
