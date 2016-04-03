dfae7ff42e1dc7d23f832f34e0c583d56dc40754
Alexandre P Francisco <aplf@tecnico.pt>
2016-04-03 23:27:37 +0100

    Update file headers.

--- net-phyloviz-project/src/net/phyloviz/project/PhylovizProjectLogicalView.java
+++ net-phyloviz-project/src/net/phyloviz/project/PhylovizProjectLogicalView.java
@@ -1,8 +1,23 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+/*-
+ * Copyright (c) 2016, PHYLOViZ Team <phyloviz@gmail.com>
+ * All rights reserved.
+ * 
+ * This file is part of PHYLOViZ <http://www.phyloviz.net/>.
+ *
+ * This program is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the
+ * Free Software Foundation, either version 3 of the License, or (at your
+ * option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * for more details.
+ * 
+ * You should have received a copy of the GNU General Public License along
+ * with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
+
 package net.phyloviz.project;
 
 import org.netbeans.api.annotations.common.StaticResource;
